909ac55e598402e0ca2df24b78f6d8f19967c18b
adoptanoob <adoptanoobinrails@gmail.com>
2014-11-15 15:43:35 +0100

    testing the create action in controller

--- app/controllers/devis_controller.rb
+++ app/controllers/devis_controller.rb
@@ -13,6 +13,17 @@ class DevisController < ApplicationController
 
   def create
     @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+    @devi = Devi.new(devi_params)
+
+    respond_to do |format|
+      if @devi.save
+        format.html { redirect_to @devi, notice: 'The devi was successfully created.' }
+        format.json { render :show, status: :created, location: @devi }
+      else
+        format.html { render :new }
+        format.json { render json: @devi.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -23,4 +34,10 @@ class DevisController < ApplicationController
 
   def detroy
   end
+
+    private
+
+    def devi_params
+      params.require(:devi).permit(:name, :plan_url)
+    end
 end
