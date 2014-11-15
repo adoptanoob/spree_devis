0012e6a64f7ca874a0ccd3e56e746a5cc40f946c
adoptanoob <adoptanoobinrails@gmail.com>
2014-11-15 17:33:04 +0100

    changed the callback_url param

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Spree::Core::Engine.routes.draw do
-  get 'devis/index'
-
-  get 'devis/new'
-
-  get 'devis/show'
-
-  get 'devis/create'
-
-  get 'devis/edit'
-
-  get 'devis/update'
-
-  get 'devis/detroy'
 
   resources :devis
 end
