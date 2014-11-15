891a9bfcd4533a9031b269470557decd2d6a98d6
adoptanoob <adoptanoobinrails@gmail.com>
2014-11-15 15:50:31 +0100

    got rid of json templates in devicontroller create action

--- app/controllers/devis_controller.rb
+++ app/controllers/devis_controller.rb
@@ -15,15 +15,11 @@ class DevisController < ApplicationController
     @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
     @devi = Devi.new(devi_params)
 
-    respond_to do |format|
       if @devi.save
         format.html { redirect_to @devi, notice: 'The devi was successfully created.' }
-        format.json { render :show, status: :created, location: @devi }
       else
         format.html { render :new }
-        format.json { render json: @devi.errors, status: :unprocessable_entity }
       end
-    end
   end
 
   def edit
