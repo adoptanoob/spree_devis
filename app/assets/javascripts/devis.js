107005054841f6e544ddd9194e159759bd6a9c63
adoptanoob <adoptanoobinrails@gmail.com>
2014-11-15 17:42:52 +0100

    copying an example app

--- app/assets/javascripts/devis.js
+++ app/assets/javascripts/devis.js
@@ -3,3 +3,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+  $('#s3-uploader').S3Uploader(
+    {
+      remove_completed_progress_bar: false,
+      progress_bar_target: $('#uploads_container'),
+      before_add: function(file) {
+        if (file.size > 30485760) {
+          alert('Maximum file size is 30 MB');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+  );
+
+  // error handling
+  $('#s3-uploader').bind('s3_upload_failed', function(e, content) {
+    return alert(content.filename + ' failed to upload.');
+  });
+});
